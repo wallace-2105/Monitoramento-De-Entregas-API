6174c4fe6508168a73621e7273265ffdebe97079
Wallace Coimbra <wallace.wcs83@gmail.com>
2026-08-27 00:20:44 -0300

    feat: add interactive delivery tracking map using Leaflet and route simulation support

--- frontend/src/pages/Map.tsx
+++ frontend/src/pages/Map.tsx
@@ -135,7 +135,7 @@ export function MapPage() {
           attributionControl={false}
         >
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
           />
           
           {isSimulating ? (
